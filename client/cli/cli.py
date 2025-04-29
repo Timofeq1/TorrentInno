970ee1ab648f4d3e5b5b5d196c06090a388b1fd8
Evgenii Bortsov <ebortsoff@yandex.ru>
2025-04-29 21:42:23 +0300

    Start implementing cli

--- client/cli/cli.py
+++ client/cli/cli.py
@@ -1,10 +1,10 @@
 import asyncio
-import datetime
-import threading
 from pathlib import Path
 import json
+import shlex
 
 from core.common.resource import Resource
+from torrentInno import TorrentInno, create_resource_json, create_resource_from_json
 
 
 def get_help_message() -> str:
@@ -19,40 +19,28 @@ def get_help_message() -> str:
         'start downloading the file associated with <resource-file.json> into the <destination-file>\n'
 
         '"share <path-to-file> <resource-file.json>" - '
-        'start sharing the existing <path-to-file> with other peers. The <resource-file.json>'
+        'start sharing the existing <path-to-file> with other peers. The <resource-file.json> '
         'is the metadata of the filet\n'
 
         '"show all" - show the status of all files\n'
 
-        '"show <file-id>" - show the status of file with <file-id>\n'
+        '"show <path>" - show the status of file at the path <path>\n'
 
         '"generate resource <file> <path-to-generated-resource>" - generate the resource json of the <file> '
         'and save the result into <path-to-generated-resource>'
     )
 
 
-def create_resource_from_file(file: Path):
+def create_resource_from_file(file: Path) -> Resource:
     with open(file, mode='r') as f:
-        resource_dict = json.load(f)
-        resource = Resource(
-            tracker_ip=resource_dict["trackerIp"],
-            tracker_port=resource_dict["trackerPort"],
-            comment=resource_dict["comment"],
-            creation_date=datetime.datetime.fromisoformat(resource_dict["creationDate"]),
-            name=resource_dict["name"],
-            pieces=[
-                Resource.Piece(
-                    sha256=piece_dict['sha256'],
-                    size_bytes=int(piece_dict['size'])
-                )
-                for piece_dict in resource_dict["pieces"]
-            ]
-        )
-        print(resource_dict, resource)
-        return resource
+        resource_json = json.load(f)
+        return create_resource_from_json(resource_json)
 
 
 class Client:
+    def __init__(self):
+        self.torrent_inno: TorrentInno = None  # TODO: initialize in start()
+
     async def start(self):
         print(
             "Welcome to TorrentInno CLI\n"
@@ -60,35 +48,97 @@ class Client:
             "To exit type \"quit\""
         )
         try:
-            self.infinite_loop()
+            await self.infinite_loop()
         except KeyboardInterrupt:
             print("Quitting")
 
-    def infinite_loop(self):
+    async def infinite_loop(self):
         while True:
             print(">", end=' ')
             line = input()
             if line == "help":
                 print(get_help_message())
                 continue
+
             if line == "quit":
                 print("Quitting")
                 break
-            tokens = line.split(' ')
+
+            tokens = shlex.split(line)
+
             if tokens[0] == "download":
                 try:
-                    destination = Path(tokens[1])
-                    resource_file = Path(tokens[2])
+                    destination = Path(tokens[1]).expanduser()
+                    resource_file = Path(tokens[2]).expanduser()
                     if destination.exists():
                         print(f"The {destination} already exists. Abort download")
                         continue
                     resource = create_resource_from_file(resource_file)
-                    # TODO: start the download somehow
+                    await self.torrent_inno.start_download_file(destination.resolve(), resource)
+                    print(f"Start downloading a file into {destination.resolve()}")
                 except Exception as e:
                     print(f"Download failed: {e}")
-                    continue
+
+            elif len(tokens) == 2 and tokens[0] == "show" and tokens[1] == "all":
+                try:
+                    states = await self.torrent_inno.get_all_files_state()
+                    for state in states:
+                        print(state)
+                except Exception as e:
+                    print(f"Something went wrong: {e}")
+
+            elif tokens[0] == "share":
+                try:
+                    destination = Path(tokens[1]).expanduser()
+                    resource_file = Path(tokens[2]).expanduser()
+                    if not destination.exists():
+                        print(f"The {destination} does not exist. Abort share")
+                        continue
+                    resource = create_resource_from_file(resource_file)
+                    await self.torrent_inno.start_share_file(destination.resolve(), resource)
+                    print(f"Start sharing file at {destination}")
+                except Exception as e:
+                    print(f"Share failed: {e}")
+
+            elif tokens[0] == "show":
+                try:
+                    destination = Path(tokens[1]).expanduser()
+                    state = await self.torrent_inno.get_state(destination.resolve())
+                    print(state)
+                except Exception as e:
+                    print(f"Fail when fetching the status of file at {tokens[0]}: {e}")
+
+            elif len(tokens) == 4 and tokens[0] == "generate" and tokens[1] == "resource":
+                try:
+                    file = Path(tokens[2]).expanduser()
+                    resource_file = Path(tokens[3]).expanduser()
+
+                    print(file.resolve())
+
+                    if not file.exists():
+                        print(f"File {file.resolve()} does not exist. Abort generation")
+                        continue
+
+                    # Start the interactive session
+                    print("Enter the comment: ")
+                    comment = input()
+
+                    print("Enter the name of the resource file: ")
+                    name = input()
+
+                    resource_json = create_resource_json(name=name, comment=comment, file_path=file)
+                    with open(resource_file, mode='w') as f:
+                        json.dump(resource_json, f, indent=4, ensure_ascii=False)
+                    print("Successfully created")
+                except Exception as e:
+                    print(f"Failed when generating the resource file: {e}")
+            else:
+                print("Unknown command")
 
 
 def main():
-    client = Client()
-    asyncio.run(client.start())
+    try:
+        client = Client()
+        asyncio.run(client.start())
+    except Exception as e:
+        print("Quitting")
